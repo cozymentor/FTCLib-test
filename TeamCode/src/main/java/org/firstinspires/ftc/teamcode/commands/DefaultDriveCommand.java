cbb9ba1738ed41745b84dd98655c890ce782ef56
cozymentor <theo.asberry@covesd.org>
2023-10-27 09:21:05 -0700

    Changed DefaultDriveCommand

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DefaultDriveCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DefaultDriveCommand.java
@@ -8,9 +8,22 @@ import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
 public class DefaultDriveCommand extends CommandBase {
 
     private final DriveSubsystem m_drive;
+    private final DoubleSupplier m_leftX;
+    private final DoubleSupplier m_leftY;
+    private final DoubleSupplier m_rightX;
+    private final DoubleSupplier m_heading;
 
     public DefaultDriveCommand(DriveSubsystem drive, DoubleSupplier leftX, DoubleSupplier leftY, DoubleSupplier rightX, DoubleSupplier heading){
         m_drive = drive;
-        drive.drive(leftX.getAsDouble(), leftY.getAsDouble(), rightX.getAsDouble(), heading.getAsDouble());
+        m_leftX = leftX;
+        m_leftY = leftY;
+        m_rightX = rightX;
+        m_heading = heading;
+        addRequirements(m_drive);
+    }
+
+    @Override
+    public void execute() {
+        m_drive.drive(m_leftX.getAsDouble(), m_leftY.getAsDouble(), m_rightX.getAsDouble(), m_heading.getAsDouble());
     }
 }
