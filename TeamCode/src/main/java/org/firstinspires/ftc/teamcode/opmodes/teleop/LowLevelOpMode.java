b41a8eb226369b4f7b6fd008b07a935fd08ebc57
cozymentor <theo.asberry@covesd.org>
2023-11-01 12:35:19 -0700

    Modified code structure and included opmode files T

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/LowLevelOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/LowLevelOpMode.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.opmodes;
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
